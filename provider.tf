bc1c7914cd6340aa67d61b1a20afbfb84352cfa3
chamu <chamukallutla@gmail.com>
2026-02-16 14:41:12 +0530

    create complete vpc

--- provider.tf
+++ provider.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket        = "chamu-terraform-state"
-    key           = "vpc/terraform.tfstate"
-    region        = "eu-west-1"
-    use_lockfile  = true
+    bucket         = "chamu-terraform-state"
+    key            = "vpc/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"   # <-- add this
+    encrypt        = true
   }
 }
 
